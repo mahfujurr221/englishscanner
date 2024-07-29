1942bba1a0f2bd7e86ae6e8ff154f1dabfef918e
mahfujurr221 <mahfujurr221@gmail.com>
2024-07-29 10:15:01 +0600

    courses, videos and lets learn database created

---
+++ app/Models/LetsLearn.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LetsLearn extends Model
+{
+    use HasFactory;
+}
